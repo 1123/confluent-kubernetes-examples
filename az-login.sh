fa298e77157f52ee6492dd6c32f2def0676b2370
Benedikt Linse <benedik@confluent.io>
2025-02-02 09:35:03 +0000

    working on workflows

--- az-login.sh
+++ az-login.sh
@@ -1,14 +1,17 @@
 set -u -e
 
+echo "Logging in to Azure"
 az login \
    --service-principal \
    -u $AZ_CLIENT_ID \
    -p $AZ_CLIENT_SECRET \
-   --tenant $AZ_TENANT_ID 
+   --tenant $AZ_TENANT_ID > /dev/null
 
+echo "Getting credentials for Kubernetes Cluster"
 az aks get-credentials \
                --resource-group $AZ_RG \
                --name $AKS_CLUSTER \
-               --overwrite-existing
+               --overwrite-existing > /dev/null 2>&1
 
-az account set --subscription $AZ_SUBSCRIPTION_ID
+echo "Setting subscription"
+az account set --subscription $AZ_SUBSCRIPTION_ID 
