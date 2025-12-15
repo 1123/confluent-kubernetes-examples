d2c91f77bbff8fbbb48678b17345b45e0845944f
Benedikt Linse <benedikt@confluent.io>
2025-12-15 13:38:29 +0100

    incrementing docker image version

--- flink/manual-install/app/build-image.sh
+++ flink/manual-install/app/build-image.sh
@@ -1,2 +1,2 @@
 ACR_NAME=benediktubuntudev
-az acr build --registry $ACR_NAME --image flink-sample-app:0.0.3 --file ./Dockerfile .
+az acr build --registry $ACR_NAME --image flink-sample-app:0.0.4 --file ./Dockerfile .
